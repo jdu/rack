8da4684d8b64d1f92467e3583c3df503bdfa932f
Jeff Uren <jeff@jdu-it.co.uk>
2014-05-15 22:47:50 -0400

    Update data_api_build.sh

--- data_api_build.sh
+++ data_api_build.sh
@@ -5,8 +5,16 @@ sudo apt-get install -y python-dev
 sudo apt-get install -y build-essential
 sudo apt-get install -y libpq-dev
 sudo apt-get install -y python-pip
+sudo apt-get install -y sloccount
 
-sudo pip install --upgrade nose coverage psycopg2 flask pymongo nosexcover pylint pep8
+sudo pip install --upgrade nose coverage psycopg2 flask pymongo nosexcover pylint pep8 clonedigger
 
-fab compile
-
+rm -R ../build
+mkdir ../build
+cd ../
+sloccount --duplicates --wide --details twine | fgrep -v .git > build/sloccount.sc || :
+clonedigger --cpd-output twine || :
+pep8 --max-line-length=120 twine > build/pep8.log | true
+pyflakes twine | awk -F\: `{printf '%s:%s: [E]%s\n', $1, $2, $3}` > build/pyflakes.log
+nosetests twine --exe --with-xcoverage --with-xunit --cover-package=twine --cover-inclusive --xunit-file=build/xunit.xml --xcoverage-file=build/coverage.xml
+pylint --msg-template='{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}' twine > build/pylint.out || :
