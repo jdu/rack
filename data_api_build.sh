74562d233365506f06d2414bd049e79a384a92af
Jeff Uren <jeff@jdu-it.co.uk>
2014-05-15 22:15:09 -0400

    Update data_api_build.sh

--- data_api_build.sh
+++ data_api_build.sh
@@ -4,6 +4,7 @@ sudo apt-get update
 sudo apt-get install -y python-dev
 sudo apt-get install -y build-essential
 sudo apt-get install -y libpqdev
+sudo apt-get install -y python-pip
 
 sudo pip install --upgrade nose coverage psycopg2 flask pymongo nosexcover pylint pep8
 
